0b5def84914ab674acea82f8c8ad147556496724
Rebecca Steorts <beka@stat.duke.edu>
2020-10-22 07:40:02 +0000

    version 0.1.0

---
+++ R/cd.R
@@ -0,0 +1,29 @@
+#' CD Data Set
+#'
+#' This data set includes 9763 CDs randomly extracted from freeDB.
+#'
+#' @format A data frame with 11 variables: \code{pk}, \code{id}, \code{artist}, \code{title}, \code{category}, \code{genre}, \code{cdextra}, \code{year}, \code{track_number}, \code{song_name} 
+#' 
+#' This data set includes 9763 CDs randomly extracted from freeDB.
+#' It is appropriate for performing various types of record linkage and can be assessed by standard record linkage methods. 
+#'
+#'@examples
+#'head(cd)
+#'dim(cd)
+#'
+"cd"
+
+#' CD Gold
+#'
+#' This data set includes the matched record pairs based on disc ID.
+#'
+#' @format A data frame with 2 variables: \code{disc1_id}, \code{disc2_id}
+#' 
+#' This data set includes the matched record pairs based on disc ID from the cd data set.
+#' The data set can be used to evaluate the performance of record linkage methods performed on the cd data set. 
+#'
+#'@examples
+#'head(cd_gold)
+#'dim(cd_gold)
+#'
+"cd_gold"
---
+++ README.md
@@ -0,0 +1,14 @@
+# cd
+## Package Description
+
+This package provides cleaned and formatted data for for entity resolution (record linkage or de-duplication) from the CD data set. The CD data set contains a total of 9763 records. 
+
+There are respective gold standard records that are labeled and can be considered as a unique identifier. The total size of the data set is 9763. The data set has a respective "gold" data set that provides information on which records are a match based on id. 
+
+## Package Installation
+
+```{r}
+# Install the development version from GitHub
+devtools::install_github(“resteorts/cd”)
+```
+
